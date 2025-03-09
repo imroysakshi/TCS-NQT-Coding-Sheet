f98a4ccada69e1cb29bc5ca6ef973c469489f987
imroysakshi <roysakshi355@gmail.com>
2025-03-09 13:32:53 +0530

    Day2

---
+++ Array/AverageOfAllElements.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class AverageOfAllElements {
+
+    static int findAverage(int arr[]){
+        int n=arr.length;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum/n;
+    }
+     public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of array: ");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    System.out.println("Enter the values in array: ");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    System.out.println(findAverage(arr));
+    sc.close();
+  }
+}
--- Array/BlockSwapAlgorithm.java
+++ Array/BlockSwapAlgorithm.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class BlockSwapAlgorithm {
-    
+
+    static void reverseArray(int arr[],int i,int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+
+    static void rotateElementsByk(int arr[],int k){
+        int n=arr.length;
+        k=(n-k)%n;
+        reverseArray(arr,0,n-1);
+        reverseArray(arr,0,k-1);
+        reverseArray(arr, k, n-1);
+    }
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the size of array: ");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    System.out.println("Enter the values in array: ");
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    System.out.println("Enter the value of k: ");
+    int k=sc.nextInt();
+    rotateElementsByk(arr,k);
+    for(int i=0;i<arr.length;i++)
+       System.out.print(arr[i]+" ");
+    sc.close();
+  }
 }
