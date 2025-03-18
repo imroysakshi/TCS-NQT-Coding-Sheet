91235961467acb6388391a4000f5113307480550
imroysakshi <roysakshi355@gmail.com>
2025-03-18 15:21:26 +0530

    Find the ASCII value of a character

---
+++ Strings/AsciiValueOfString.java
@@ -0,0 +1,10 @@
+public class AsciiValueOfString {
+
+    
+    public static void main(String[] args) {
+        char c='J';
+        // String s = "race cars";
+        int i=c;
+        System.out.println(i);
+    }
+}
