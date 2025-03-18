99d5c77603a9645c17218f44c2fd439e1f803ea3
imroysakshi <roysakshi355@gmail.com>
2025-03-18 15:37:56 +0530

    Remove all vowels from the String.

--- Strings/RemoveAllVowelFromStr.java
+++ Strings/RemoveAllVowelFromStr.java
@@ -3,7 +3,7 @@ public class RemoveAllVowelFromStr {
     static void removeVowel(String s){
         StringBuilder ans=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u')
+            if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U')
                 continue;
             else
                 ans.append(s.charAt(i));
@@ -11,7 +11,7 @@ public class RemoveAllVowelFromStr {
         System.out.println(ans);
     }
     public static void main(String[] args) {
-        String s = "Take u forward is Awesome";
+        String s = "Sakshi Roy";
         removeVowel(s);
     }
 }
